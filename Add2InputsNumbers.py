5ab03ae8354e95fbd06777d34d973301d393410f
Ajmal Aju <dev@ajmalaju.com>
2019-02-18 12:41:39 +0530

    Simple Operations

---
+++ Add2InputsNumbers.py
@@ -0,0 +1,10 @@
+
+# Program For Adding 2 user inputs(Numbers)
+A = input("Enter a input")
+B = input("Enter a input")
+
+# Add two numbers
+Add = float(A) + float(B)
+
+# Display the result
+print('The sum of {0} and {1} is {2}'.format(A, B, Add))
